6a924a032bb06eaf9f91fbc8b015c4c8eb577bfc
gmcln-rct <producersama@gmail.com>
2020-05-15 18:47:57 -0400

    Add basic slider

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import Header from './components/Header';
+import ExpenseSlider from './components/ExpenseSlider';
+
 function App() {
   return (
     <div className="App">
+
+      <Header/>
+      <ExpenseSlider />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/ExpenseSlider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseSlider = () => {
+    return (
+        <div>
+              <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
+
+        </div>
+    )
+}
+
+export default ExpenseSlider; 
