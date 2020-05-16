16dd612be8e2e0f2c6e6e06675e0f86bb530258d
gmcln-rct <producersama@gmail.com>
2020-05-16 18:28:10 -0400

    Build out context

--- src/context/BudgetContext.js
+++ src/context/BudgetContext.js
@@ -1,5 +1,28 @@
 import { createContext } from 'react';
 
-const BudgetContext = createContext();
+export const BudgetContext = createContext({
+    expense: 0,
+    savings: 0
+});
+
+export function BudgetContextProvider(props) {
+
+    const budgetContext = useContext(BudgetContext);
+    const [expense, setExpense] = useState(budgetContext.expense);
+    const [savings, setSavings] = useState(budgetContext.savings);
+
+    const provider = {
+        expense,
+        savings,
+    }
+
+    return (
+        <BudgetContext.Provider value={provider}>
+            {props.children}
+        </BudgetContext.Provider>
+    )
+
+
+}
 
 export default BudgetContext;
