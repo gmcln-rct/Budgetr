0267962c7ea2235b7758ffe42c935402870585af
gmcln-rct <producersama@gmail.com>
2020-05-17 17:33:01 -0400

    Updates

---
+++ precache-manifest.2015913caf069d899a20d02202c0d5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0817508b71c39c44ff4c88bd6bdbbd",
+    "url": "/Budgetr/index.html"
+  },
+  {
+    "revision": "a4847340ee8aa1b38fcc",
+    "url": "/Budgetr/static/css/main.dbd3593b.chunk.css"
+  },
+  {
+    "revision": "2508ffe660df80b59d1b",
+    "url": "/Budgetr/static/js/2.42faff76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budgetr/static/js/2.42faff76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4847340ee8aa1b38fcc",
+    "url": "/Budgetr/static/js/main.c7a4ce6c.chunk.js"
+  },
+  {
+    "revision": "fc85a8fc0f05c67c2fc0",
+    "url": "/Budgetr/static/js/runtime-main.8b1c8d28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Budgetr/static/media/logo.5d5d9eef.svg"
+  }
+]);
