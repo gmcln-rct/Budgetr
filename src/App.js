8fbd1deb411b152d533ef0414058619e91f17aca
gmcln-rct <producersama@gmail.com>
2020-05-20 19:54:30 -0400

    Work on button

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ function App() {
 
 
             <InputForm />
-            <ExpenseSavings />
 
+            <Button />
       </BudgetContextProvider>
       </main>
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -7,8 +7,21 @@ export const Button = () => {
     // const budgetContext = useContext(BudgetContext);
     const { toggleResults, setToggleResults } = useContext(BudgetContext);
 
+    const budgetContext = useContext(BudgetContext);
+
+    // const toggle = (e) => {
+    //     e.preventDefault();
+    //     budgetContext.setLoggedIn(true);
+    // };
+
+    const currentToggle = toggleResults;
+
+    if (!budgetContext.loggedIn || budgetContext.toggleResults) {
+        return null;
+    }
+
     return (
-        <button onClick={() => setToggleResults(true)}>
+        <button onClick={() => setToggleResults(!currentToggle)}>
             Confirm
         </button>
     )
